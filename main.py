73b703872873cfc26cd3f989cfc88f1b443fe022
Medina <luisangelmedna00@gmail.com>
2022-08-02 11:39:16 -0400

    Arreglos

--- main.py
+++ main.py
@@ -89,14 +89,13 @@ bot.write(cc[0])
 print(f'CC: {cc[0]}')
 
 bot.write(cc[1])
-print(f"CC: {cc[1]}")
-
 bot.write(cc[2][2:4])
 print(f"MMYY: {cc[1]}/{cc[2][2:4]}")
 
 bot.write(cc[3])
 print(f"CVC: {cc[3]}")
+
 bot.pause(0.2)
 bot.scroll(-500)
-bot.pause(1)
+bot.pause(1.5)
 bot.locate('review_order', check=True)
