1826fffdfcaec8a9e8af8b28dddaf43267cdb91c
Medina <luisangelmedna00@gmail.com>
2022-08-02 20:57:57 -0400

    Arreglos

--- main.py
+++ main.py
@@ -13,76 +13,81 @@ url = 'https://www.everlane.com/products/unisex-diamond-stitch-sock-lavender'
 
 def preparar_worksplace():
 
-    # Necesitamos hacer que abra el Navegador, por defecto: chrome
+    if bot.locate('everlane', click=False):
+        print('Usando el entorno de trabajo actual')
 
-    bot.press('win')
-    bot.pause(0.2)
-    bot.write('chrome')
-    bot.pause(0.2)
-    bot.press('enter')
+    else:
+        print('Preparando entorno de trabajo')
+        # Necesitamos hacer que abra el Navegador, por defecto: chrome
 
-    # Abrimos Everlane
+        bot.press('win')
+        bot.pause(0.2)
+        bot.write('chrome')
+        bot.pause(0.2)
+        bot.press('enter')
 
-    bot.pause(1)
-    bot.press_both('ctrl', 'l')
-    bot.write(url)
-    bot.press('enter')
+        # Abrimos Everlane
 
-    # Quitamos la casilla del 10% descuento
+        bot.pause(1)
+        bot.press_both('ctrl', 'l')
+        bot.write(url)
+        bot.press('enter')
 
-    #bot.locate('descuento', check=True, click=False)
-    #bot.locate('descuento1', wait=1) or bot.locate('descuento2')
+        # Quitamos la casilla del 10% descuento
 
-    # Escogemos la Talla, agregamos al carrito y hacemos checkout
-    bot.locate('sock', wait=2, check=True, click=False)
-    bot.scroll(-200)
-    bot.locate('talla_M', wait=0.5) or bot.locate('talla_L')
-    bot.locate('agregar_carrito', wait=0.5)
-    bot.locate('your_cart', check=True, click=False)
-    bot.locate('checkout')
+        #bot.locate('descuento', check=True, click=False)
+        #bot.locate('descuento1', wait=1) or bot.locate('descuento2')
 
-    # Llenamos los datos de forma random y vamos haciendo scroll
-    id = generar_identidad()
+        # Escogemos la Talla, agregamos al carrito y hacemos checkout
+        bot.locate('sock', wait=2, check=True, click=False)
+        bot.scroll(-200)
+        bot.locate('talla_M', wait=0.5) or bot.locate('talla_L')
+        bot.locate('agregar_carrito', wait=0.5)
+        bot.locate('your_cart', check=True, click=False)
+        bot.locate('checkout')
 
-    bot.locate('enter_email', check=True, click=False)
-    bot.locate('email_address')
-    bot.write(id[2])
-    bot.locate('continue_to_shipping', wait=0.1) or bot.press('enter')
-    bot.scroll(-200)
-    bot.locate('shipping', check=True, click=False)
-    bot.locate('full_name', wait=0.5)
-    print('Nombre')
-    bot.pause(0.10)
-    bot.write(f"{id[0]} {id[1]}")
-    bot.press('tab') # Saltamos la casilla de Full Name
-    bot.press('tab') # Saltamos la casilla de Country
-    bot.pause(0.10)
-    print('Direccion')
-    bot.write(str(random.randint(1500,1700)))
-    bot.press('tab') # Saltamos la casilla de Street Address
-    bot.press('tab') # Saltamos la casilla de Apartment
-    bot.press('tab') # Saltamos la casilla de PO Box
-    bot.pause(0.10)
-    print('Ciudad')
-    bot.write('Doral')
-    bot.press('tab') # Saltamos la casilla de City
-    bot.write('fl')
-    bot.pause(0.10)
-    print('Estado')
-    bot.press('enter')
-    bot.press('tab') # Saltamos la casilla de State
-    bot.pause(0.10)
-    print('Zip Code')
-    bot.write('33191')
-    bot.press('tab') # Saltamos la casilla de Zip Code
-    bot.press('tab') # Saltamos la casilla de Phone Number
-    bot.pause(0.10)
-    bot.scroll(-300)
-    bot.locate('see_shipping_options', 1)
-    bot.locate('select_shipping_option', check=True)
-    bot.locate('continue_to_payment', wait=0.5) or bot.press('enter')
-    bot.pause(0.50)
-    bot.scroll(-500)
+        # Llenamos los datos de forma random y vamos haciendo scroll
+        id = generar_identidad()
+
+        bot.locate('enter_email', check=True, click=False)
+        bot.locate('email_address')
+        bot.write(id[2])
+        bot.locate('continue_to_shipping', wait=0.1) or bot.press('enter')
+        bot.scroll(-200)
+        bot.locate('shipping', check=True, click=False)
+        bot.locate('full_name', wait=0.5)
+        print('Nombre')
+        bot.pause(0.10)
+        bot.write(f"{id[0]} {id[1]}")
+        bot.press('tab') # Saltamos la casilla de Full Name
+        bot.press('tab') # Saltamos la casilla de Country
+        bot.pause(0.10)
+        print('Direccion')
+        bot.write(str(random.randint(1500,1700)))
+        bot.press('tab') # Saltamos la casilla de Street Address
+        bot.press('tab') # Saltamos la casilla de Apartment
+        bot.press('tab') # Saltamos la casilla de PO Box
+        bot.pause(0.10)
+        print('Ciudad')
+        bot.write('Doral')
+        bot.press('tab') # Saltamos la casilla de City
+        bot.write('fl')
+        bot.pause(0.10)
+        print('Estado')
+        bot.press('enter')
+        bot.press('tab') # Saltamos la casilla de State
+        bot.pause(0.10)
+        print('Zip Code')
+        bot.write('33191')
+        bot.press('tab') # Saltamos la casilla de Zip Code
+        bot.press('tab') # Saltamos la casilla de Phone Number
+        bot.pause(0.10)
+        bot.scroll(-300)
+        bot.locate('see_shipping_options', 1)
+        bot.locate('select_shipping_option', check=True, click=False)
+        bot.locate('continue_to_payment', wait=0.5) or bot.press('enter')
+        bot.pause(0.50)
+        bot.scroll(-500)
 
 def livear(cc_namso):
     print(f"Current {cc_namso}")
@@ -174,30 +179,32 @@ def livear(cc_namso):
 
         bot.pause(0.5)
         bot.locate('place_order')
-    
-# Logica
 
-def main():
-
-    ccs = """
-5217295329270668|11|2023|334
-5217295329288637|11|2023|780
-    """
-
-    preparar_worksplace()
-
-    cc_for_use = crear_lista(ccs)
-
-    for cc in cc_for_use:
-        
-        livear(cc)
         bot.pause(5)
+
         if bot.locate('thanks') or bot.locate('live') or bot.locate('survey'):
             print(f'CC: {cc} - Live!')
             preparar_worksplace()
         elif bot.locate('error1'):
             print('There was a problem processing your card. Please call your card issuer or try a different card.')
 
+    
+# Logica
+
+def main():
+
+    ccs = """
+5217295329203123|11|2023|550
+5217295329275634|11|2023|017
+5217295329234417|11|2023|434
+    """
+
+    cc_for_use = crear_lista(ccs)
+
+    for cc in cc_for_use:
+        
+        livear(cc)
+        
 if __name__ == '__main__':
     # preparar_worksplace()
     try: 
